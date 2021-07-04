7d8ffa9bbe6a1ff1497eeb080671f1f735504efb
Joe Christensen <joechristensenwork@gmail.com>
2021-07-04 13:24:49 -0700

    commit tree a5125196ea89f314223fa2d010b9db043c626b22

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 const init = () => {
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let id = event.target.searchByID.value;
+
+
+    fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(data => {
+      for(thing of data) {
+        if(thing.id.toString() === id) {
+          let title = document.querySelector("h4");
+          let summary = document.querySelector("p");
+          title.textContent = thing.title;
+          summary.textContent = thing.summary;
+        }
+      }
+    });
+  });
   
 }
 
